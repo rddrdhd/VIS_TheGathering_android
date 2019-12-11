9a1a5374c35e143835febc297b269209e4cd1a86
Leona <zur0037@vsb.cz>
2019-12-11 15:13:21 +0100

    first data from my API

--- app/src/main/java/com/example/vis_thegathering/AddAdActivity.java
+++ app/src/main/java/com/example/vis_thegathering/AddAdActivity.java
@@ -4,12 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URL;
+import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.ion.Ion;
 
 public class AddAdActivity extends AppCompatActivity {
 
@@ -17,22 +15,15 @@ public class AddAdActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_ad);
-        try {
-            URL yahoo = new URL("http://vis-zur0037.php5.cz/indexAPI.php/ads/new?aTitle=title&aType=card&aCardId=234&aQuantity=2&aPrice=12&aNote=Ahoj");
-            BufferedReader in = new BufferedReader(
-                    new InputStreamReader(
-                            yahoo.openStream()));
 
-            String inputLine;
+        Ion.with(getApplicationContext()).load("http://www.vis-zur0037.php5.cz/indexAPI.php/decks").asString().setCallback(new FutureCallback<String>() {
+            @Override
+            public void onCompleted(Exception e, String result) {
 
-            while ((inputLine = in.readLine()) != null)
-                Log.e("errNot", inputLine);
+                Log.e("notErr", result);
+            }
+        });
 
-            in.close();
-        }
-        catch (Exception e) {
-            Log.e("err",e.toString());
-        }
     }
 }
 //Boolean switchState = simpleSwitch.isChecked();
